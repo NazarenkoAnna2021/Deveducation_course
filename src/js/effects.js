85ad1871e1607cc809b58d9e6588447d05035923
NazarenkoAnna2021 <93328568+NazarenkoAnna2021@users.noreply.github.com>
2021-12-09 20:18:04 +0200

    update effects

--- src/js/effects.js
+++ src/js/effects.js
@@ -1,8 +1,8 @@
 class ButtonsEffects {
-    normalizeButton(event){ event.target.classList.remove('click-down')}
-    setMouseOver(event) {event.target.classList.add('mouse-over')}
-    setMouseOut(event) {event.target.classList.remove('mouse-over')}
-    clickedButton(event) {event.target.classList.add('click-down')}
+    normalizeButton = (event) => event.target.classList.remove('click-down') 
+    setMouseOver = (event) =>  event.target.classList.add('mouse-over') 
+    setMouseOut = (event) => event.target.classList.remove('mouse-over') 
+    clickedButton = (event) => event.target.classList.add('click-down') 
 }
 
 export const buttonsEffects = new ButtonsEffects();
